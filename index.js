f87596df22d590b39eb6384bcb8c6ff29a278347
Mario <modulor@hotmail.es>
2021-04-06 10:35:10 -0500

    create a basic POST request

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.use(express.json());
+
+app.post('/mercadopago', (req, res) => {
+  res.send(`hi ${req.body.name}`);
+  console.log(req.body);
 });
 
 app.listen(3000, () => {
